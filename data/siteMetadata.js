bc03448a5f3fad4a4f95b66dd9dc2d50eccd684d
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-03-14 21:20:59 +0530

    Update siteMetadata.js added all the site metadata details

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Tech Council',
+  author: 'Swaubhik Chakraborty',
+  headerTitle: 'Tech Council',
+  description: 'All Technical Clubs of CIT Kokrajhar',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
