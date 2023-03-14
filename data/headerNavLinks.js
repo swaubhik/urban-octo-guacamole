7e29cba3c8b7b5a5d33222bf762b10996bd385c5
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-03-14 21:21:36 +0530

    Update headerNavLinks.js update and added new nav links

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,4 +1,5 @@
 const headerNavLinks = [
+  { href: '/clubs', title: 'Clubs' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
