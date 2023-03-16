6de786154bb5f482dbac365c9ba21fb4b8fe9703
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-03-16 15:14:13 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -15,16 +15,14 @@ export default function Footer() {
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteMetadata.author}</div>
+          <div>Made with 💙 by {siteMetadata.author}</div>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link>
+          {siteMetadata.description}
         </div>
       </div>
     </footer>
