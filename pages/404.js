3be406a43a1afe4849652c1c5215025db772141c
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-03-16 15:16:08 +0530

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,17 @@
 import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
+import Image from 'next/image'
+import ErrorDonut from '/public/static/images/Error-donut.svg'
 
 export default function FourZeroFour() {
   return (
     <>
       <PageSEO title={`Page Not Found - ${siteMetadata.title}`} />
-      <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
+      <div className="flex flex-col items-center justify-center">
+        <Image src="/static/images/Error-donut.svg" alt="Error Donut" width={500} height={200} />
+      </div>
+      <div className="flex flex-col items-start justify-start md:mt-16 md:flex-row md:items-center md:justify-center md:space-x-6">
         <div className="space-x-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
             404
