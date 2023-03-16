c4eb3575e9beb8c5ee9f17d062fd98e6820a497a
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-03-16 15:14:55 +0530

    Update _app.js Added netlify identity

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import '@fontsource/inter/variable-full.css'
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
+import Script from 'next/script'
 
 import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
@@ -20,12 +21,24 @@ export default function App({ Component, pageProps }) {
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" async></script>
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
+      <Script id="netlifyWidget" strategy="afterInteractive">
+        {` if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }`}
+      </Script>
     </ThemeProvider>
   )
 }
